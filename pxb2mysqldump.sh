507cc6d29563b7a433f84e68ac06feaf312752f5
Greg Cockburn <gergnz@gmail.com>
2013-01-09 07:03:18 +1100

    more comments, add -A to usage

--- pxb2mysqldump.sh
+++ pxb2mysqldump.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
-# A script to turn Percona Xtrabackup to seperate database dumps.
+# A script to turn Percona Xtrabackup to seperate or one large database dump.
+set -e
 
-##getopts
-## backupdir
-## user (mysql)
-
-##Check I am root
-
-
+## Print basic usage
 usage() {
   me=`basename $0`
-  echo "$me [-u username] [-h] [directory]"
+  echo "$me [-u username] [-A] [-h] [directory]"
   exit 1
 }
 
-#if [ "$EUID" -ne "0" ]; then
-#  echo "you are not running as root" >&2
-#  usage
-#fi
+## Check I am root
+if [ "$EUID" -ne "0" ]; then
+  echo "you are not running as root" >&2
+  usage
+fi
 
+## Set some defaults
 backupdir=`pwd`
 runasuser='mysql'
 alldatabases=FALSE
 
+## Grab the options, and setup variables
 while getopts ":u:hA" opt; do
   case $opt in
     u)
@@ -47,23 +45,28 @@ while getopts ":u:hA" opt; do
   esac
 done
 
+## Set the backupdir if supplied
 if [ -n "${1}" ]; then
   backupdir=${1}
 fi
 
+## Safe guard
+## TODO: Use a special override incase this is what people want to do.
 if [ "${backupdir}" = "/var/lib/mysql" ]; then
   echo "You probably don't want to run this in /var/lib/mysql." >&2
   exit 1
 fi
 
+## Does the supplied directory exist?
 if [ ! -d ${backupdir} ]; then
   echo "The provided dir: ${backupdir}, doesn't exist" >&2
   exit 1
 fi
 
+## Dump all the logs, config, etc, in a unique directory
 temp=`mktemp -d -p $backupdir`
 
-# Create a minimal my.cnf
+## Create a minimal my.cnf
 cat > $temp/my.cnf << EOF
 [mysqld]
 skip-networking
@@ -82,10 +85,13 @@ err-log=$temp/mysqld.log
 pid-file=$temp/mysqld.pid
 EOF
 
+## Make all the files owned by the user we are going to run as.
 chown -R ${runasuser} ${backupdir}
 
+## Start mysql
 mysqld_safe --defaults-file=${temp}/my.cnf
 
+## Check that mysql has innodb
 have_innodb=$(mysql -S ${temp}/mysql.sock -NB -e "show variables like 'have_innodb'"|awk '{print $2}')
 if [ "$have_innodb" != "YES" ]; then
   echo "InnoDB was not detected, exiting" >&2
@@ -93,6 +99,7 @@ if [ "$have_innodb" != "YES" ]; then
 fi
 
 if [ "${alldatabases}" == "TRUE" ]; then
+  ## Full database dump
   echo "Dumping all databases..."
   mysqldump -S ${temp}/mysql.sock --all-databases --quick --single-transaction | gzip > ${backupdir}/alldatabases.sql.gz
   echo "Done"
@@ -100,6 +107,7 @@ if [ "${alldatabases}" == "TRUE" ]; then
   echo "# Example:"
   echo "# zcat ${backupdir}/alldatabases.sql.gz | mysql -uroot -p "
 else 
+  ## Just dump useful databases
   for database in $(mysql -S ${temp}/mysql.sock -BN -e "show databases"|grep -Ev 'mysql|test|information_schema'); do
     echo "Found: $database"
     echo -n "Dumping $database to: $database.sql..."
@@ -111,6 +119,7 @@ else
   done
 fi
 
+## Destory the mysql running
 kill $(ps -fC mysqld|grep "${backupdir}"|awk '{print $2}')
 
 # vim:syntax=bash
